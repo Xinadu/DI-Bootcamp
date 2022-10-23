5b699165cce7fe5ff27b53ae387d282a92cf9e60
Xinadu <antony.rahman.180@gmail.com>
2022-10-23 10:14:57 +0300

    improved code

--- Week2/Day4/Learning_Functions/DailyChallenge/script.js
+++ Week2/Day4/Learning_Functions/DailyChallenge/script.js
@@ -1,32 +1,24 @@
-function frame() {
-    let space = ' ',hasComma = true;
-    while(hasComma){
-        let words = prompt("enter strings seperated with comma: ");
-        if(!words.includes(",")){
-            alert("missing comma!!")
-        }else{
-            alert("Accepted")
-            hasComma = false;
-            let newWords = words.replaceAll(/\s/g, '').split(",").filter(item => item);
-            let star = '*', dif, longest = 0,wordLen;
-            for(let i = 0; i< newWords.length; i++){
-                if(newWords[i].length > longest){
-                    longest = newWords[i].length
-                }       
-            }
-            
-            console.log(star.repeat(longest+4));
-        
-            for(let i = 0; i < newWords.length ; i++){ 
-                wordLen = newWords[i].length;
-                dif = longest - wordLen;
-                console.log(`${star}${space}${newWords[i]}${space.repeat(dif)}${space}${star}`);
-            }
-                console.log(star.repeat(longest+4))
-            
+const promptInput = prompt("Enter comma separated words");
+const words = promptInput.split(",");
+function findLongestWord() {
+    //find the longest word
+    let max = 0;
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > max) {
+            max = words[i].length;
         }
     }
+    return max;
+}
+function printWords(words,max) {
+    firstAndLast(max);
+    for (let word of words) {
+        console.log(`* ${word + ' '.repeat(max - word.length)} *`);
+    }
+    firstAndLast(max);
 
 }
-
-frame()
+function firstAndLast(max) {
+    console.log('*'.repeat(max + 4));
+}
+printWords(words,findLongestWord());
