e9b4a005ca528cc8b2897062e940a854115b25b4
Xinadu <antony.rahman.180@gmail.com>
2022-10-25 08:33:29 +0300

    ExercisesXP upload

--- Week3/Day1/Intro_to_DOM/ExercisesXP/script.js
+++ Week3/Day1/Intro_to_DOM/ExercisesXP/script.js
@@ -1,32 +1,72 @@
-//div
+// //div
 
-const firstDiv = document.body.children[0]
-const divOne = document.body.firstElementChild;
-console.log(divOne);
-//ul
+// const firstDiv = document.body.children[0]
+// const divOne = document.body.firstElementChild;
+// console.log(divOne);
+// //ul
 
-const firstUL = document.body.children[1];
-const ulOne = firstDiv.firstElementChild;
+// const firstUL = document.body.children[1];
+// const ulOne = firstDiv.firstElementChild;
 
-//second li "Pete"
+// //second li "Pete"
 
-const secondLi = firstUL.children[1];
-const LiTwo = firstUL.lastElementChild;
+// const secondLi = firstUL.children[1];
+// const LiTwo = firstUL.lastElementChild;
 
-LiTwo.textContent = "Richard";
+// LiTwo.textContent = "Richard";
 
-console.log(LiTwo);
+// console.log(LiTwo);
 
-const secondUL = document.body.children[2];
-
-
-console.log(secondUL);
-
-const LiOne = firstUL.children[0]
-LiOne.textContent = "Anton"
-
-const SndLiOne = secondUL.children[0]
-SndLiOne.textContent = "Anton"
+// const secondUL = document.body.children[2];
 
 
+// console.log(secondUL);
 
+// const LiOne = firstUL.children[0]
+// LiOne.textContent = "Anton"
+
+// const SndLiOne = secondUL.children[0]
+// SndLiOne.textContent = "Anton"
+
+// const trdLiOne = secondUL.children[1]
+// secondUL.removeChild(trdLiOne)
+
+
+
+
+
+
+
+
+// document.body.firstElementChild.style.backgroundColor = "lightblue";
+// document.body.firstElementChild.style.padding = "50px";
+// document.body.style.fontSize = "x-large";
+
+// const li1 = document.body.children[1];
+
+
+
+// const liChild = li1.children[0];    
+
+// const liChild2 = li1.children[1];
+
+
+
+// liChild.style.display = "none";
+
+// liChild2.style.border = "thick solid #0000FF";
+
+
+
+
+
+// const div1 = document.body.firstElementChild;
+// div1.setAttribute("id", "socialNetworkNavigation");
+// console.log(div1);
+
+// const createdlist = document.createElement("li");
+// const createdtext = document.createTextNode("Logout");
+// createdlist.appendChild(createdtext);
+// const uList = div1.firstElementChild;
+
+// uList.appendChild(createdlist);
